265fee7a2010dfb88aeab608099481d92dd56f63
KenAbraham18 <124kabraham@frhsd.com>
2023-12-12 09:37:11 -0500

    gdosfdnklsfd klsvdnklfdjljlsfdjhljhlsfdjhlsfdjksf

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 $(document).ready(function(){
     //alert("jquery loaded");
-    //$(h3).css({border: "3px solid blue"});
-    //$(".wrapper").css({border: "3px solid red"});
-    //$("#clients").css({border: "3px solid yellow"});
-
-    $("#contact-methods").css({border: "2px solid red"}).next().css({border: "2px solid green"}).closest("section").css({border:"2px solid blue"});
+/*$("h3").css({border: "3px solid blue"});
+$(".wrapper").css({border: "3px solid red"});
+$("#clients").css({border: "3px solid yellow"});
 
 
+$("#contact-methods").css({border: "2px solid red"}).next().css({border: "2px solid green"}).closest("section").css({border: "2px solid blue"});*/
+var tweet = "<div style='margin: 20px 0; padding: 10px; background: #eee'>The big fight live: Ham vs Cheese!</div>";
+$("#tweets div").append(tweet);
 
-var tweet = "<div style=',margin: 20px 0; passing: 10px; background: #eee'>The big fight live: Ham vs Cheese! </div>";
-
-$("#tweets div").append(tweet)
-
-$("section").wrap("<div");
+$("section").wrap("<div>");
 $("section").unwrap();
 $("section").wrapAll("<div>");
 
@@ -22,4 +19,5 @@ $("#points-of-sale").empty();
 $("#contact img").removeAttr("alt");
 $("#contact img").attr("alt", "location");
 
-$("#social-nav").css("top", "-125px").css("left", 50px);
+$("#social-nav").css("top", "-125px").css("left", "50px");
+});
